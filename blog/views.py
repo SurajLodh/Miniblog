1fc49128681c7d040c9f7212f50b065a6bc4b71b
SurajLodh <surajlodh10962@gmail.com>
2021-04-28 02:19:00 +0530

    first commit

---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def Home(request):
+
+    return render(request, 'home.html')
+
+def About(request):
+
+    return render(request, 'about.html')
+
+def Contact(request):
+
+    return render(request, 'contact.html')
